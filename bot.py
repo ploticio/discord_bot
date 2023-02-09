580cc0bd9a3eb8c2f4c16c7e01968561c27f0c36
Darren <darrentran001@gmail.com>
2023-02-08 16:52:39 -0800

    User input word counter implemented, bad word counter renamed

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def run_bot():
                 if re.search(r'\b%s\b' % word, message.content.lower()):
                     await message.add_reaction("🤨")
                     await message.add_reaction("📸")
-                    await message.reply(f"🚨 WOAH 🚨 😱😱😱 {message.guild.owner.mention} {message.guild.owner.mention} {message.guild.owner.mention}")
+                    await message.reply(f"🚨 WOAH 🚨 😱😱😱")
 
         await bot.process_commands(message)
 
@@ -33,41 +33,54 @@ def run_bot():
     @bot.command()
     async def bad(ctx, user: discord.User):
         final_count = {word: 0 for word in config.NAUGHTY_WORDS}
-        await _read_prior(ctx, user, final_count)
-        await _read_current(ctx, user, final_count)
+        
+        await _read_prior(ctx, user, config.NAUGHTY_WORDS, final_count)
+        await _read_current(ctx, user, config.NAUGHTY_WORDS, final_count)
         
         for key in final_count:
             await ctx.send(f"{user.mention} has said {key} {final_count[key]} times")
 
         await _add_reacts(ctx, final_count)
+
+    @bot.command()
+    async def count(ctx, user: discord.User, *args):
+        final_count = {"word_list": 0}
+        words = {"word_list": args}
+
+        await _read_prior(ctx, user, words, final_count)
+        await _read_current(ctx, user, words, final_count)
+
+        await ctx.send(f"{user.mention} has said {', '.join(args)} {sum(final_count.values())} times")
         
     # Text history read in as JSON
-    async def _read_prior(ctx, user, counts) -> None:
+    async def _read_prior(ctx, user, words, counts) -> None:
         await ctx.send("Reading history...")
         for filename in os.listdir("data/"):
             with open(os.path.join("data/", filename), "r") as f:
                 data = json.load(f)
             for subdict in data['messages']:
                 if subdict['author']['id'] == str(user.id): 
-                    for key in config.NAUGHTY_WORDS:
-                        for word in config.NAUGHTY_WORDS[key]:
+                    for key in words:
+                        for word in words[key]:
                             found = len(re.findall(r'\b%s\b' % word, subdict['content'].lower()))
                             if found > 0:
                                 counts[key] += found
 
     # Most recent messages
-    async def _read_current(ctx, user, counts):
+    async def _read_current(ctx, user, words, counts):
         for channel in ctx.guild.text_channels:
-                try:
-                    messages = [message async for message in channel.history(limit=200) 
-                                if message.author == user]
-                    for m in messages:
-                        for key in config.NAUGHTY_WORDS:
-                            for word in config.NAUGHTY_WORDS[key]:
-                                if re.search(r'\b%s\b' % word, m.content.lower()):
-                                    counts[key] += 1
-                except:
-                    continue
+            try:
+                messages = [message async for message in channel.history(limit=200) 
+                            if message.author == user]
+                for m in messages:
+                    for key in words:
+                        for word in words[key]:
+                            found = len(re.findall(r'\b%s\b' % word, m.content.lower()))
+                            if found > 0:
+                                counts[key] += found
+            except Exception as e:
+                print(f"Exception Occured: {e}")
+                continue
     
     # Adds reactions based on count
     async def _add_reacts(ctx, count):
@@ -77,10 +90,10 @@ def run_bot():
             await msg.add_reaction("👍")
         elif num == 1:
             await msg.add_reaction("🤨")
-        elif num > 1 and num < 10:
+        elif num > 1 and num < 25:
             await msg.add_reaction("🤨")
             await msg.add_reaction("📸")
-        elif num >= 10 and num < 15:
+        elif num >= 25 and num < 50:
             await msg.add_reaction("💀")
         else:
             await msg.add_reaction("😱")
